b9a158b530af4a7290319a343038a7025ec88da2
pydarl <85401455+pydarl@users.noreply.github.com>
2023-08-28 16:03:45 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # pydarl
+   # <your GitHub username>
    # Input:
    # p, principal amount
    # t, time period in years
